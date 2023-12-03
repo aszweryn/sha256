3dbf87f12449f7ec3794dc817c5a98d699bb9087
aszweryn <101809061+aszweryn@users.noreply.github.com>
2023-12-03 17:04:43 +0100

    feat: helper functions for input conversion

--- sha256.py
+++ sha256.py
@@ -1,2 +1,50 @@
 def is_palindrome(test_param: str) -> bool:
     return True
+
+def translate(message):
+    """Translates strings into binary number returned as a list of integers"""
+    chars = [ord(c) for c in message] # ord translates string to Unicode value
+
+    bytes = []
+    for char in chars:
+        bytes.append(bin(char)[2:].zfill(8)) # replacing '0b' indicator with 0 
+
+    # returning the value as the list of integers
+    bitlist = []
+    for byte in bytes:
+        for bit in byte:
+            bitlist.append(int(bit))
+    return bitlist
+
+def bin2Hex(val):
+    """For presenting hash values in hexadecimal notation, takes a list of 32 bits as an input"""
+    val = ''.join([str(x) for x in val])
+
+    bins = []
+    for d in range(0, len(val), 4):
+        bins.append('0b' + val[d:d+4]) # converting the list back to '0bx' format
+
+    hexes = ''
+    for b in bins:
+        hexes += hex(int(b, 2))[2:]
+    return hexes
+
+def zerosFill(bits, length=8, endian='LE'):
+    """Useful for formatting data for usage with Python methods"""
+    l = len(bits)
+
+    if endian == 'LE':
+        for _ in range(l, length):
+            bits.append(0)
+    else:
+        while l < length:
+            bits.insert(0, 0)
+            l = len(bits)
+    return bits
+
+def divider(bits, chunk_length=8):
+    """Dividing the message into desired chunks"""
+    chunked = []
+    for b in range(0, len(bits), chunk_length):
+        chunked.append(bits[b:b+chunk_length])
+    return chunked

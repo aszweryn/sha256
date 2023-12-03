de12ca446c9df8d66681d0e05c596e60ed777a19
aszweryn <101809061+aszweryn@users.noreply.github.com>
2023-12-03 17:32:02 +0100

    feat: initialization function

--- sha256.py
+++ sha256.py
@@ -1,13 +1,14 @@
 def is_palindrome(test_param: str) -> bool:
     return True
 
+
 def translate(message):
     """Translates strings into binary number returned as a list of integers"""
     chars = [ord(c) for c in message]  # ord translates string to Unicode value
 
     bytes = []
     for char in chars:
-        bytes.append(bin(char)[2:].zfill(8))  # replacing '0b' indicator with 0 
+        bytes.append(bin(char)[2:].zfill(8))  # replacing '0b' indicator with 0
 
     # returning the value as the list of integers
     bitlist = []
@@ -16,18 +17,20 @@ def translate(message):
             bitlist.append(int(bit))
     return bitlist
 
+
 def bin2Hex(val):
     """For presenting hash values in hexadecimal notation, takes a list of 32 bits as an input"""
     val = "".join([str(x) for x in val])
 
     bins = []
     for d in range(0, len(val), 4):
-        bins.append("0b" + val[d:d+4])  # converting the list back to '0bx' format
+        bins.append("0b" + val[d : d + 4])  # converting the list back to '0bx' format
 
-    hexes = "" 
+    hexes = ""
     for b in bins:
         hexes += hex(int(b, 2))[2:]
     return hexes
+
 
 def zerosFill(bits, length=8, endian="LE"):
     """Useful for formatting data for usage with Python methods"""
@@ -42,10 +45,22 @@ def zerosFill(bits, length=8, endian="LE"):
             l = len(bits)
     return bits
 
+
 def divider(bits, chunk_length=8):
     """Dividing the message into desired chunks"""
     chunked = []
     for b in range(0, len(bits), chunk_length):
-        chunked.append(bits[b : b+chunk_length])
+        chunked.append(bits[b : b + chunk_length])
     return chunked
 
+
+def initialize(values):
+    """Initializing the algorithm and converting values into a list of bits"""
+    bins = [bin(int(v, 16))[2:] for v in values]
+    words = []
+    for binary in bins:
+        word = []
+        for b in binary:
+            word.append(int(b))
+        words.append(zerosFill(word, 32, 'BE'))
+    return words

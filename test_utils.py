7aae8e12434eab1271995b05f322737292161df2
unknown <101809061+aszweryn@users.noreply.github.com>
2023-12-07 17:34:02 +0100

    feat: failing unit tests

--- test_utils.py
+++ test_utils.py
@@ -1,42 +1,21 @@
 import pytest
 from sha256.utils_preprocessing import translate, bin2Hex, bin2Int, padding
 
-
 def test_translate():
-    assert translate("") == []
-    assert translate("A") == [0, 1, 0, 0, 0, 0, 0, 1]
-    assert translate("Hello") == [
-        0, 1, 0, 1, 0, 0, 1, 0,
-        0, 1, 1, 0, 0, 1, 0, 0,
-        0, 1, 1, 0, 0, 1, 1, 1,
-        0, 1, 1, 0, 1, 1, 1, 1,
-        1, 0, 0, 1, 1, 0, 0, 1,
-    ]
+    result = translate("Hi")
+    assert result == [0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0, 1]
 
 
 def test_bin2Hex():
-    assert bin2Hex([0, 1, 0, 0, 0, 0, 0, 1]) == "21"
-    assert bin2Hex([
-        0, 1, 0, 1, 0, 0, 1, 0,
-        0, 1, 1, 0, 0, 1, 0, 0,
-        0, 1, 1, 0, 0, 1, 1, 1,
-        0, 1, 1, 0, 1, 1, 1, 1,
-        1, 0, 0, 1, 1, 0, 0, 1,
-    ]) == "51487799"
+    result = bin2Hex([1, 0, 0, 0, 1, 0, 1])
+    assert result == "45"
 
 
 def test_bin2Int():
-    assert bin2Int([0, 1, 0, 0, 0, 0, 0, 1]) == 33
-    assert bin2Int([
-        0, 1, 0, 1, 0, 0, 1, 0,
-        0, 1, 1, 0, 0, 1, 0, 0,
-        0, 1, 1, 0, 0, 1, 1, 1,
-        0, 1, 1, 0, 1, 1, 1, 1,
-        1, 0, 0, 1, 1, 0, 0, 1,
-    ]) == 1346525561
+    result = bin2Int([1, 1, 0, 1])
+    assert result == 13
 
 
 def test_padding():
-    actual_output = padding(0)
-    expected_prefix = b'\x80\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'
-    assert actual_output.startswith(expected_prefix)
+    result = padding(0)
+    assert result == b'\x80\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'

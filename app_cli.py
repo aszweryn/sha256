178d776e11493aea2d2a670bc2c1d8221b9bc1c2
Marcel <marcel.piotrowski5543@gmail.com>
2023-12-07 12:56:53 +0100

    fix: lint

--- app_cli.py
+++ app_cli.py
@@ -2,7 +2,10 @@ import argparse
 from sha256 import sha256
 
 parser = argparse.ArgumentParser("cli_sha256_hash")
-parser.add_argument("input_string", help="A string will be hashed using sha256 function and printed at the output.")
+parser.add_argument(
+    "input_string",
+    help="A string will be hashed using sha256 function and printed at the output.",
+)
 args = parser.parse_args()
 
 hashed = sha256.hash(args.input_string)

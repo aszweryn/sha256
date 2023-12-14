39cb0f961bb694563c59f65975f794f9c747f896
aszweryn <101809061+aszweryn@users.noreply.github.com>
2023-12-14 18:11:51 +0100

    feat: file handling capabilities

--- app_cli.py
+++ app_cli.py
@@ -1,12 +1,41 @@
 import argparse
 from sha256 import sha256
 
-parser = argparse.ArgumentParser("cli_sha256_hash")
-parser.add_argument(
-    "input_string",
-    help="A string will be hashed using sha256 function and printed at the output.",
-)
-args = parser.parse_args()
 
-hashed = sha256.hash(args.input_string)
-print(hashed)
+def hash_and_print(input_string, output_file=None):
+    hashed = sha256.hash(input_string)
+
+    if output_file:
+        with open(output_file, "w") as file:
+            file.write(hashed)
+        print(f"Hash result has been written to {output_file}")
+    else:
+        print(hashed)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser("sha256_cli")
+    parser.add_argument(
+        "input_string",
+        help="The input message or file path to be hashed.",
+    )
+    parser.add_argument(
+        "--output_file", "-o", help="Specify the output file to write the hash result."
+    )
+    parser.add_argument(
+        "--file",
+        "-f",
+        action="store_true",
+        help="If provided, treat the input string as a file path",
+    )
+    args = parser.parse_args()
+
+    if args.file:
+        try:
+            with open(args.input_string, "r") as file:
+                input_content = file.read().strip()
+                hash_and_print(input_content, args.output_file)
+        except FileNotFoundError:
+            print(f"Error: File not found - {args.input_string}")
+    else:
+        hash_and_print(args.input_string, args.output_file)
